a173425887dd2c4d6ace2023e7eb861ae3e0a52a
yt1849-code <yt1849@srmist.edu.in>
2026-03-01 22:32:20 +0530

    Add files via upload

---
+++ uc1.java
@@ -0,0 +1,6 @@
+public class uc1 {
+  
+public static void main(String[] args) {
+  System.out.println("OPPS");
+}
+}
---
+++ uc2.java
@@ -0,0 +1,15 @@
+public class uc2 {
+
+  public static void main(String[] args) {
+    System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"   ***** ");
+    System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" **      ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "+"  **     ");
+    System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"    ***  ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
+    System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"      ** ");
+    System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+" *****  ");
+
+  }
+}
