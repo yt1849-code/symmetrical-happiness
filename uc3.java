2853f8a06ec192bd31ce8a260c121396cbae135e
yt1849-code <yt1849@srmist.edu.in>
2026-03-01 22:33:11 +0530

    Add files via upload

---
+++ uc3.java
@@ -0,0 +1,14 @@
+public class uc3 {
+  public static void main(String[] args) {
+    System.out.println(String.join(" ","   ***    ","   ***    ","******    ","   ***** "));
+    System.out.println(String.join(" "," **   **  "," **   **  ","**    **  "," **      "));
+    System.out.println(String.join(" ","**     ** ","**     ** ","**     ** ","**       "));
+    System.out.println(String.join(" ","**     ** ","**     ** ","**    **  ","  **     "));
+    System.out.println(String.join(" ","**     ** ","**     ** ","******    ","    ***  "));
+    System.out.println(String.join(" ","**     ** ","**     ** ","**        ","      ** "));
+    System.out.println(String.join(" ","**     ** ","**     ** ","**        ","       **"));
+    System.out.println(String.join(" "," **   **  "," **   **  ","**        ","      ** "));
+    System.out.println(String.join(" ","   ***    ","   ***    ","**        "," *****   "));
+    }
+  
+}
